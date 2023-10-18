ad97c33760caedc253ec727d54f0b4478a6f0f8b
TheClemAub <62135502+TheClemAub@users.noreply.github.com>
2023-10-18 12:46:57 +0200

    Update challengesGen.java

--- src/main/java/Models/challengesGen.java
+++ src/main/java/Models/challengesGen.java
@@ -22,6 +22,8 @@ public class challengesGen {
     public challengesGen() {
     }
 
+    int intTransit = 0;
+
     public List<Exercise> genChallenge(String skill) {
         List<Exercise> exercises = new ArrayList();
         Random random = new Random();
@@ -29,21 +31,90 @@ public class challengesGen {
         for (int i = 0; i < 7; i++) {
             Exercise exercise = new Exercise();
             int answer = 0;
-
+            int typeExo = 0;
+            int op1 = 0;
+            int op2 = 0;
+            int nbrAlea = 0;
+            int nbrTemp = 0;
+            
             switch (skill){
                 case "Additionner un entier à un autre pour obtenir 10" :
+                    op1 = random.nextInt(9)+1;                   
+                    op2 = 10;
+                    answer = op2 - op1;
+                    nbrAlea = random.nextInt(89)+10;
+                    nbrTemp = (nbrAlea + 1) * 10;
+                    exercise.setContent("Combien faut-il ajouter à " + nbrAlea + op1 + " pour obtenir " + nbrTemp + " ?");
+                    typeExo = 1;
+                break;
+                case "Additionner un entier à un autre pour obtenir 100" :
+                    op1 = random.nextInt(99)+1;                   
+                    op2 = 100;
+                    answer = op2 - op1;                            
+                    nbrAlea = random.nextInt(10)+1 * 10**(random.nextInt(5)+2);        
+                    nbrTemp = nbrAlea - answer;                 
+                    exercise.setContent("Combien faut-il ajouter à " + nbrTemp + " pour obtenir " + nbrAlea + " ?");
+                    typeExo = 2;
+                break;
+                 case "Comparer deux entiers" :  
+                    op1 = random.nextInt(100000000 - 100000) + 100000;
+                    int limB = (int) (op1 - 0.00001 * op1);
+                    int limH = (int) (op1 + 0.00001 * op1);
+                    switch (random.nextInt(2)) {
+                        case 0:
+                            op2 = random.nextInt(limH - limB) + limB;
+                            break;
+                        case 1:
+                            op2 = random.nextInt(limH - limB) + limH;
+                            break;
+                    }                   
+                    if (op1 < op2){
+                        exercise.setContent("Quel est la valeur de ??? dans la comparaison : " + op1 + " < ???");
+                        intTransit = op1;
+                        answer = op2;
+                    } else {
+                        exercise.setContent("Quel est la valeur de ??? dans la comparaison : " + "??? < " + op1);    
+                        answer = op2;
+                        intTransit = op1;
+                    }
+                    typeExo = 3;
+                break;
+                    case "Encadrer un entier entre deux entiers" :  
+                    op1 = random.nextInt(100000 - 10000) + 10000;
+                    int limB = (int) (op1 - 0.00001 * op1);
+                    int limH = (int) (op1 + 0.00001 * op1);
+                    switch (random.nextInt(2)) {
+                        case 0:
+                            op2 = random.nextInt(limH - limB) + limB;
+                            break;
+                        case 1:
+                            op2 = random.nextInt(limH - limB) + limH;
+                            break;
+                    }                   
+                    if (op1 < op2){
+                        answer = (int) ((op2 - op1)/2 + op1);
+                        intTemp = (int) ((op1 - op2)/2 + op1);
+                    } else {
+                       answer = (int) ((op2 - op1)/2 + op2);
+                        intTemp = (int) ((op1 - op2)/2 + op2);
+                                            
+                    exercise.setContent("Quelle valeur est comprise entre " + op1 + " et " + op2 + " ?");
+                    typeExo = 4;
+                break;
+                case "Connaitre les critères de division par 2" :
                     do {
                         answer = random.nextInt(80) + 20;
                     } while (answer % 2 != 0);
                     exercise.setContent("Quel nombre est divisible par 2?");
-                    break;
+                    typeExo = 0;
+                break;
                 default:
                     exercise.setContent("Default");
-                    break;
+                break;
             }
 
             exercise.setAnswer(Integer.toString(answer));
-            exercise.setOptions(generateOptions(answer, 0));
+            exercise.setOptions(generateOptions(answer, typeExo));
 
             exercises.add(exercise);
         }
@@ -60,15 +131,33 @@ public class challengesGen {
 
         while (options.size() < 4) {
             switch (ID){
-                case 0:
+                case 0: // 1 seul nbr divisible par 2
                     option = answer + random.nextInt(20) - 5;
                     do {
                         option = answer + random.nextInt(20) - 5;
                     } while (option < 0 || option%2 == 0);
-                    break;
+                break;
+                case 1: // cplmnt à 10
+                     option = answer + random.nextInt(5) - 2;
+                break;
+                    case 2: // cplmnt à 100   
+                    do {
+                     option = answer + (random.nextInt(5) - 2)*10;
+                    } while (option == answer);
+                break;
+                    case 3: // comparaison entier   
+                    if (intTransit < answer){
+                        option = intTransit - (random.nextInt(10)+1)**(random.nextInt(4)+1);
+                    } else {
+                        option = intTransit + (random.nextInt(10)+1)**(random.nextInt(4)+1);
+                    }
+                break;
+                    case 4: // comparaison entier   
+                    option = intTransit - (random.nextInt(10)+1)**(random.nextInt(4)+1);
+                break;
                 default:
                     option = answer + random.nextInt(5) - 200;
-                    break;
+                break;
             }
             if (!options.contains(Integer.toString(option))) {
                 options.add(Integer.toString(option));
