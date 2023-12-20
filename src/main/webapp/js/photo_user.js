4665f266e64c75667164e6061f518bab0ddd2088
TheClemAub <62135502+TheClemAub@users.noreply.github.com>
2023-12-20 14:37:23 +0100

    Create photo_user.js

---
+++ src/main/webapp/js/photo_user.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var userLog = document.getElementById('userLog');
+    var userText = userLog.textContent.trim();
+
+    var basePath = "./images/";  // Préfixe pour le chemin des images
+    var imgFileName;
+
+    switch (userText) {
+        case "Texte 1":
+            imgFileName = "photo1.jpg";
+            break;
+        case "Texte 2":
+            imgFileName = "photo2.jpg";
+            break;
+        case "Texte 3":
+            imgFileName = "photo3.jpg";
+            break;
+        case "Texte 4":
+            imgFileName = "photo4.jpg";
+            break;
+        case "Texte 5":
+            imgFileName = "photo5.jpg";
+            break;
+        default:
+            imgFileName = "default.jpg"; // Nom de fichier par défaut
+            break;
+    }
+
+    var imgSrc = basePath + imgFileName; // Construire le chemin complet de l'image
+
+    var imageElement = document.createElement('img');
+    imageElement.src = imgSrc;
+    document.body.appendChild(imageElement); // Ajoute l'image au corps de la page
+});
