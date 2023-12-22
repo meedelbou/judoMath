34e27f75d62a245ded600260693aebd636ae3fc5
TheClemAub <62135502+TheClemAub@users.noreply.github.com>
2023-12-22 09:33:38 +0100

    Update photo_user.js

--- src/main/webapp/js/photo_user.js
+++ src/main/webapp/js/photo_user.js
@@ -1,36 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var userLog = document.getElementById('userLog');
-    var userText = userLog.textContent.trim();
+    var userLogs = document.getElementsByClassName('userLog');
+    var basePath = "./images/userImgs/";
 
-    var basePath = "./images/userImgs/";  // Préfixe pour le chemin des images
-    var imgFileName;
+    for (var i = 0; i < userLogs.length; i++) {
+        var userText = userLogs[i].textContent.trim();
+        var imgFileName;
 
-    switch (userText) {
-        case "clemeleve":
-            imgFileName = "xana.jpg";
-            break;
-        case "Texte 2":
-            imgFileName = "photo2.jpg";
-            break;
-        case "Texte 3":
-            imgFileName = "photo3.jpg";
-            break;
-        case "Texte 4":
-            imgFileName = "photo4.jpg";
-            break;
-        case "Texte 5":
-            imgFileName = "photo5.jpg";
-            break;
-        default:
-            imgFileName = "pikachu.jpg"; // Nom de fichier par défaut
-            break;
-    }
+        switch (userText) {
+            case "clemeleve":
+                imgFileName = "xana.jpg";
+                break;
+            case "Texte 2":
+                imgFileName = "photo2.jpg";
+                break;
+            case "Texte 3":
+                imgFileName = "photo3.jpg";
+                break;
+            case "Texte 4":
+                imgFileName = "photo4.jpg";
+                break;
+            case "Texte 5":
+                imgFileName = "photo5.jpg";
+                break;
+            default:
+                imgFileName = "pikachu.jpg";
+                break;
+        }
 
-    var imgSrc = basePath + imgFileName; // Construire le chemin complet de l'image
-
-    var imageElements = document.getElementsByClassName('img_user');
-    
-    for (var i = 0; i < imageElements.length; i++) {
-        imageElements[i].src = imgSrc;
+        var imgSrc = basePath + imgFileName;
+        var imageElements = document.getElementsByClassName('img_user');
+        for (var j = 0; j < imageElements.length; j++) {
+            imageElements[j].src = imgSrc;
+        }
     }
 });
+
